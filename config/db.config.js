7b1f58cf8efa95ef8c58857f8557202a5fbe9f80
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2022-12-09 22:45:10 -0300

    MySQL configuration

---
+++ config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = { 
+    HOST: "localhost", 
+    USER: "root", 
+    SENHA: "123456", 
+    DB: "testdb", 
+    dialeto: "mysql", 
+    pool: { 
+      máx: 5, 
+      min: 0, 
+      aquisição: 30000, 
+      ocioso: 10000 
+    } 
+  }; 
